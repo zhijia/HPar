01b1079be9fc630a5a0ee3b8f92da5ff08f66048
Zhijia Zhao <zzhao@cs.wm.edu>
2012-09-21 15:21:45 -0700

    handle several real pages w/ certain no. of threads

---
+++ src/main/java/org/jsoup/nodes/EndComment.java
@@ -0,0 +1,39 @@
+package org.jsoup.nodes;
+
+
+// zhijia added
+public class EndComment extends Node {
+	private static final String ENDCOMMENT_KEY = "endComment";
+	
+    public EndComment(String data, String baseUri) {
+        super(baseUri);
+        attributes.put(ENDCOMMENT_KEY, data);
+    }
+	
+    public String nodeName() {
+        return "#endComment";
+    }
+    
+    public String getData() {
+        return attributes.get(ENDCOMMENT_KEY);
+    }
+    
+    void outerHtmlHead(StringBuilder accum, int depth, Document.OutputSettings out) {
+        if (out.prettyPrint())
+            indent(accum, depth, out);
+        accum             
+                .append(getData())
+                .append("-->");
+    }
+    
+    void outerHtmlTail(StringBuilder accum, int depth, Document.OutputSettings out) {}
+
+    public String toString() {
+        return outerHtml();
+    }
+    
+	@Override
+	public String nodeType() {
+		return "EndComment";
+	}
+}
